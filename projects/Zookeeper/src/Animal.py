412fd156e52c3c1b2edd205b4d124da05ac48474
Christian Dataro <christian@phrc.live>
2025-01-23 14:04:41 +0800

    feat: ✨  finished a stage 1 of 4 - implementing class-based approach

---
+++ projects/Zookeeper/src/Animal.py
@@ -0,0 +1,29 @@
+from constants import DEER_MESSAGE, LION_MESSAGE, BAT_MESSAGE
+
+
+class Animal:
+    pass
+
+
+class Deer(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return DEER_MESSAGE
+
+
+class Bat(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return BAT_MESSAGE
+
+
+class Lion(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return LION_MESSAGE
